e262eb8225fbe08d2724a873ca62ab2fb811fbdf
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-22 22:08:58 +0300

    Day02 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,17 +1,55 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var h = 0
+        var deep = 0
+
+        for (rawString in input) {
+            val split: List<String> = rawString.split(" ")
+            val command = split[0]
+            val value = split[1].toInt()
+
+            when (command) {
+                "forward" -> h += value
+                "down" -> deep += value
+                "up" -> deep -= value
+                else -> throw IllegalArgumentException(command)
+            }
+        }
+
+
+        return h * deep
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var h = 0
+        var deep = 0
+        var aim = 0
+
+        for (rawString in input) {
+            val split: List<String> = rawString.split(" ")
+            val command = split[0]
+            val value = split[1].toInt()
+
+            when (command) {
+                "forward" -> {
+                    h += value
+                    deep += aim * value
+                }
+                "down" -> aim += value
+                "up" -> aim -= value
+                else -> throw IllegalArgumentException(command)
+            }
+        }
+
+        return h * deep
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    check(part2(testInput) == 900)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
 }
