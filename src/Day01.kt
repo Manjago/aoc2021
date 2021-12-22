eaf16d50d4a025bb75a9c46d29eb2e1f0a267545
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-22 17:52:43 +0300

    Day01 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,48 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+
+        var firstEnter = true
+        var incCounter = 0
+        var prevValue = 0
+        for (strNumber in input) {
+            if (firstEnter) {
+                prevValue = strNumber.toInt()
+                firstEnter = false
+            } else {
+                val currentValue = strNumber.toInt()
+                if (currentValue > prevValue) {
+                    ++incCounter
+                }
+                prevValue = currentValue
+            }
+        }
+
+        return incCounter
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        if (input.size < 3) {
+            return -1
+        }
+
+        var prevWindow = input[0].toInt() + input[1].toInt() + input[2].toInt()
+        var incCounter = 0
+
+        for (i in 3 until input.size) {
+            val currentWindow = prevWindow + input[i].toInt() - input[i- 3].toInt()
+            if (currentWindow > prevWindow) {
+                ++incCounter
+            }
+            prevWindow = currentWindow
+        }
+
+        return incCounter
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
