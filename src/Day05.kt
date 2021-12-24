2b8cac0366dd657263ef3637c94c09eae87f916c
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-24 22:28:55 +0300

    Day05 solved

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,5 +1,6 @@
 import java.lang.Integer.max
-import java.lang.Integer.min
+import java.lang.Integer.signum
+import kotlin.math.abs
 
 fun main() {
 
@@ -11,36 +12,29 @@ fun main() {
     ) {
         fun isVertical() = x1 == x2
         fun isHorizontal() = y1 == y2
+        fun is45() = abs(x1 - x2) == abs(y1 - y2)
+        fun distance() = max(abs(x1 - x2), abs(y1 - y2)) + 1
     }
 
     fun stringsToLines(input: List<String>): List<Line> = input.asSequence().map {
         val list4 = it.split("->").asSequence().flatMap { it.split(",") }.map { it.trim().toInt() }.toList()
         Line(list4[0], list4[1], list4[2], list4[3])
-    }.filter { it.isVertical() || it.isHorizontal() }
-        .toList()
+    }.toList()
 
     class Matrix(
         val dimX: Int, val dimY: Int,
         private val data: Array<IntArray> = Array(dimY) { IntArray(dimX) }
     ) {
         fun drawLine(line: Line) {
+            val stepX = signum(line.x2 - line.x1)
+            val stepY = signum(line.y2 - line.y1)
 
-            when {
-                line.isVertical() -> {
-                    val startY = min(line.y1, line.y2)
-                    val endY = max(line.y1, line.y2)
-                    for (i in startY..endY) {
-                        ++data[i][line.x1]
-                    }
-                }
-                line.isHorizontal() -> {
-                    val startX = min(line.x1, line.x2)
-                    val endX = max(line.x1, line.x2)
-                    for (i in startX..endX) {
-                        ++data[line.y1][i]
-                    }
-                }
-                else -> error("unexpected line $line")
+            var x = line.x1
+            var y = line.y1
+            repeat(line.distance()) {
+                ++data[y][x]
+                x += stepX
+                y += stepY
             }
         }
 
@@ -57,41 +51,30 @@ fun main() {
         }
     }
 
-    fun part1(input: List<String>): Int {
-
-        val rawLines = stringsToLines(input)
-
-        val maxX = rawLines.maxOf { max(it.x1, it.x2) }
-        val maxY = rawLines.maxOf { max(it.y1, it.y2) }
-        val minX = rawLines.minOf { min(it.x1, it.x2) }
-        val minY = rawLines.minOf { min(it.x1, it.x2) }
-        val shiftX = minX
-        val shiftY = minY
-        val dimX = maxX - shiftX + 1
-        val dimY = maxY - shiftY + 1
-
-        val lines = rawLines.asSequence().map {
-            Line(
-                it.x1 - shiftX, it.y1 - shiftY,
-                it.x2 - shiftX, it.y2 - shiftY
-            )
-        }.toList()
-
-        val matrix = Matrix(dimX, dimY)
+    fun corePart(lines: List<Line>): Int {
+        val maxX = lines.maxOf { max(it.x1, it.x2) }
+        val maxY = lines.maxOf { max(it.y1, it.y2) }
+        val matrix = Matrix(maxX + 1, maxY + 1)
         lines.forEach { matrix.drawLine(it) }
 
         return matrix.countGreaterOrEqualThen(2)
     }
 
+    fun part1(input: List<String>): Int {
+        return corePart(stringsToLines(input).filter { it.isVertical() || it.isHorizontal() })
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        return corePart(stringsToLines(input).filter { it.isVertical() || it.isHorizontal() || it.is45() })
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 5)
+    val part1 = part1(testInput)
+    check(part1 == 5) { "part1 test = $part1" }
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
-    println(part1(input))
-    println(part2(input))
+    check(part1(input) == 5373)
+    println(part2(input)) // 21481 too low
 }
