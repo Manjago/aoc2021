65fe42775a48e6edb2d1b984765e405e6ca8facb
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-25 16:58:27 +0300

    Day06 one star

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,32 @@
 fun main() {
+
+    val limit = 80
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val data = input[0].split(",").asSequence().map { it.toInt() }.toMutableList()
+
+        var turn = 0
+        while(true) {
+
+            val finish = data.size
+            for(i in 0 until finish) {
+                val item = data[i]
+                if (item == 0) {
+                    data[i] = 6
+                    data.add(8)
+                } else {
+                    data[i] = item - 1
+                }
+            }
+
+            ++turn
+            if (turn == limit) {
+                break
+            }
+        }
+
+        return data.size
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +35,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
+    val part1 = part1(testInput)
+    check(part1 == 5934) {"test part1 = $part1"}
 
     val input = readInput("Day06")
-    println(part1(input))
-    println(part2(input))
+    val realPart1 = part1(input)
+    check(realPart1 == 349549) {"real part1 = $realPart1"}
+    println("part2 = " + part2(input))
 }
