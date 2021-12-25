466634c3cbc83a5efce57dfa680439c9e6df5cf4
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-25 22:14:00 +0300

    Day07 one start

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,6 +1,16 @@
+import java.lang.Math.abs
+
 fun main() {
+
+    fun cost(data: List<Int>, pos: Int): Int = data.sumOf { abs(pos - it) }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val data = input[0].split(",").asSequence().map { it.toInt() }.toList()
+        var pretender = Integer.MAX_VALUE
+        for (i in data.indices) {
+            pretender = pretender.coerceAtMost(cost(data, i))
+        }
+        return pretender
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +19,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day07_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 37)
 
     val input = readInput("Day07")
-    println(part1(input))
+    val realPart1 = part1(input)
+    check(realPart1 == 355521)
     println(part2(input))
 }
