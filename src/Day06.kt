a84fb63fddacc3b2eaae66eaa11dc7d51131130e
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-25 20:16:54 +0300

    Day06 solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,16 +1,31 @@
 fun main() {
 
-    val limit = 80
+    fun optimalSolution(input: List<String>, limit: Int): Long {
+        val age = LongArray(9)
+        input[0].split(",").asSequence().map { it.toInt() }.forEach {
+            ++age[it]
+        }
 
-    fun part1(input: List<String>): Int {
+        repeat(limit) {
+            val temp = age[0]
+            for(i in 0..7) {
+                age[i] = age[i+1]
+            }
+            age[8] = temp
+            age[6] += temp
+        }
 
+        return age.asSequence().sum()
+    }
+
+    fun naiveSolution(input: List<String>, limit: Int): Int {
         val data = input[0].split(",").asSequence().map { it.toInt() }.toMutableList()
 
         var turn = 0
-        while(true) {
+        while (true) {
 
             val finish = data.size
-            for(i in 0 until finish) {
+            for (i in 0 until finish) {
                 val item = data[i]
                 if (item == 0) {
                     data[i] = 6
@@ -29,17 +44,23 @@ fun main() {
         return data.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        return optimalSolution(input, 80)
+    }
+
+    fun part2(input: List<String>): Long {
+        return optimalSolution(input, 256)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
     val part1 = part1(testInput)
-    check(part1 == 5934) {"test part1 = $part1"}
+    check(part1 == 5934L) {"test part1 = $part1"}
+    val part2 = part2(testInput)
+    check(part2 == 26984457539L) {"test part2 = $part2"}
 
     val input = readInput("Day06")
     val realPart1 = part1(input)
-    check(realPart1 == 349549) {"real part1 = $realPart1"}
-    println("part2 = " + part2(input))
+    check(realPart1 == 349549L) {"real part1 = $realPart1"}
+    println("real part2 = " + part2(input))
 }
