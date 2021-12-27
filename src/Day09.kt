1fcaacca9a72410f83b982fa677ab1e5a78a09cd
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-27 17:01:48 +0300

    Day09 one gold star

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,6 +1,41 @@
 fun main() {
+
+    fun inBound(x: Int, bound: Int) : Boolean = x in 0 until bound
+
+    fun isLessThan(data: Array<IntArray>, width: Int, height: Int, x: Int, y: Int, pretender: Int) : Boolean =
+        if (inBound(x, width) && inBound(y, height)) {
+            pretender < data[y][x]
+        } else {
+            true
+        }
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val width = input[0].length
+        val height = input.size
+
+        val data = Array(height) {IntArray(width)}
+        for(i in input.indices) {
+            data[i] = input[i].toCharArray().asSequence().map { it.toString().toInt() }.toList().toIntArray()
+        }
+
+        var sum = 0
+        for(y in 0 until height) {
+            for (x in 0 until  width) {
+
+                val pretender = data[y][x]
+                if (
+                    isLessThan(data, width, height, x - 1, y, pretender) &&
+                    isLessThan(data, width, height, x + 1, y , pretender) &&
+                    isLessThan(data, width, height, x, y - 1, pretender) &&
+                    isLessThan(data, width, height, x, y + 1, pretender)
+                        ) {
+                    sum += pretender + 1
+                }
+            }
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +44,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    check(part1(testInput) == 1)
+    val testPart1 = part1(testInput)
+    check(testPart1 == 15) {"test part1 = $testPart1"}
 
     val input = readInput("Day09")
     println(part1(input))
