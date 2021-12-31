93e4de87372ac2252cfa052eabe3dab26a44588a
Kirill Temnenkov <kirill@temnenkov.com>
2021-12-31 11:05:33 +0300

    Day10 one gold star

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,7 +1,52 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun firstIllegalChar(command: String) : Char? {
+
+        val stack = ArrayDeque<Char>()
+
+        command.toCharArray().forEach {
+             when (it) {
+                 '(', '[', '{', '<' -> stack += it
+                 ')' -> if (stack.isEmpty() || stack.last() != '(') {
+                     return it
+                 } else {
+                     stack.removeLast()
+                 }
+                 '}' -> if (stack.isEmpty() || stack.last() != '{') {
+                     return it
+                 } else {
+                     stack.removeLast()
+                 }
+                 ']' -> if (stack.isEmpty() || stack.last() != '[') {
+                     return it
+                 } else {
+                     stack.removeLast()
+                 }
+                 '>' -> if (stack.isEmpty() || stack.last() != '<') {
+                     return it
+                 } else {
+                     stack.removeLast()
+                 }
+                 else -> error("bad char $it")
+             }
+
+        }
+
+        return null
     }
+
+    fun costOfChar(char: Char?) : Int = when (char) {
+        null -> 0
+        ')' -> 3
+        ']' -> 57
+        '}' -> 1197
+        '>' -> 25137
+        else -> error("bad char in cost $char")
+    }
+
+    fun part1(input: List<String>): Int = input.asSequence().map {
+        costOfChar(firstIllegalChar(it))
+    }.sum()
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,7 +54,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 1)
+    val testPart1 = part1(testInput)
+    check(testPart1 == 26397) {"test part1 = $testPart1"}
 
     val input = readInput("Day10")
     println(part1(input))
