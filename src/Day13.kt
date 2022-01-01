840d9ae2c224cf69580b21173adcd5cbc4ce7c7b
Kirill Temnenkov <kirill@temnenkov.com>
2022-01-01 15:25:22 +0300

    Day13 parser

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,5 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
+
+        val points = input.asSequence().filter { it.isNotEmpty() && it[0] != 'f' }
+            .map {
+                val coord = it.split(",")
+                Point(coord[0].toInt(), coord[1].toInt())
+            }.toList()
+        val maxX = points.maxOf { it.x }
+        val maxY = points.maxOf { it.y }
+        val board = IntBoard(maxX + 1, maxY + 1)
+        points.forEach { board[it] = 1 }
+
+        val commands = input.asSequence().filter {
+            it.isNotEmpty() && it[0] == 'f'
+        }.map {
+            val tokens = it.substringAfter("fold along ").split("=")
+            when(tokens[0]) {
+                "x" -> Point(tokens[1].toInt(), -1)
+                "y" -> Point(-1, tokens[1].toInt())
+                else -> error("bad tokens from $tokens")
+            }
+        }.toList()
+
+        println(board.display())
+        println(commands)
+
         return input.size
     }
 
