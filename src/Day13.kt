e47f990d570ececb235b718cbf05f7380a7efcb1
Kirill Temnenkov <kirill@temnenkov.com>
2022-01-01 16:11:19 +0300

    Day13 one gold star

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,4 +1,56 @@
 fun main() {
+
+    fun IntBoard.foldX(leftLen: Int): IntBoard {
+        val rightLen = this.width - leftLen - 1
+        if (leftLen < rightLen) {
+            error("can not fold by x $leftLen")
+        }
+
+        val foldedBoard = IntBoard(leftLen, height)
+
+        /*
+        width = 5
+        x = 2
+        leftlen = 2
+        rightlen = 2
+        0 1 2 3 4
+        0 1
+         */
+
+        for(j in 0 until height) {
+            for (i in 0 until leftLen) {
+                val point = Point(i, j)
+                foldedBoard[point] = this[point] + this[Point(width - i - 1, j)]
+            }
+        }
+
+        return foldedBoard
+    }
+
+    fun IntBoard.foldY(topLen: Int): IntBoard {
+        val bottomLen = this.height - topLen - 1
+        if (topLen < bottomLen) {
+            error("can not fold by y $topLen")
+        }
+
+        val foldedBoard = IntBoard(width, topLen)
+
+        for(j in 0 until topLen) {
+            for (i in 0 until width) {
+                val point = Point(i, j)
+                foldedBoard[point] = this[point] + this[Point(i, height - j - 1)]
+            }
+        }
+
+        return foldedBoard
+    }
+
+    fun IntBoard.fold(point: Point): IntBoard = if (point.y == -1) {
+        this.foldX(point.x)
+    } else {
+        this.foldY(point.y)
+    }
+
     fun part1(input: List<String>): Int {
 
         val points = input.asSequence().filter { it.isNotEmpty() && it[0] != 'f' }
@@ -15,17 +67,16 @@ fun main() {
             it.isNotEmpty() && it[0] == 'f'
         }.map {
             val tokens = it.substringAfter("fold along ").split("=")
-            when(tokens[0]) {
+            when (tokens[0]) {
                 "x" -> Point(tokens[1].toInt(), -1)
                 "y" -> Point(-1, tokens[1].toInt())
                 else -> error("bad tokens from $tokens")
             }
         }.toList()
 
-        println(board.display())
-        println(commands)
+        val foldedBoard = board.fold(commands.first())
 
-        return input.size
+        return foldedBoard.all().count { foldedBoard[it] > 0 }
     }
 
     fun part2(input: List<String>): Int {
@@ -34,7 +85,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day13_test")
-    check(part1(testInput) == 1)
+    val testPart1 = part1(testInput)
+    check(testPart1 == 17) { "test part1 = $testPart1 "}
 
     val input = readInput("Day13")
     println(part1(input))
