3ba64cf6a8a650820124c128de8849a7f59fe657
Kirill Temnenkov <kirill@temnenkov.com>
2022-01-03 13:26:27 +0300

    Graph refactoring

--- src/graph/Graph.kt
+++ src/graph/Graph.kt
@@ -1,6 +1,8 @@
 package graph
 
 interface Graph<V> {
-    fun addEdge(from: V, to: V)
+    data class VertexWithEdgeWeight<V>(val vertex: V, val weight: Int)
+    fun addEdge(from: V, to: V, weight: Int = 0)
     fun neighbours(vertex: V) : Sequence<V>
+    fun neighboursWithEdgeWeight(vertex: V) : Sequence<VertexWithEdgeWeight<V>>
 }
--- src/graph/GraphSimple.kt
+++ src/graph/GraphSimple.kt
@@ -1,18 +1,26 @@
 package graph
 
-data class GraphSimple<T>(private val data: MutableMap<T, MutableSet<T>> = mutableMapOf()) : Graph<T> {
+data class GraphSimple<T>(private val data: MutableMap<T, MutableSet<Graph.VertexWithEdgeWeight<T>>> = mutableMapOf()) :
+    Graph<T> {
 
-    override fun addEdge(from: T, to: T) {
+    override fun addEdge(from: T, to: T, weight: Int) {
         if (!data.containsKey(from)) {
             data[from] = mutableSetOf()
         }
-        data[from]!!.add(to)
+        data[from]!!.add(Graph.VertexWithEdgeWeight(to, weight))
     }
 
     override fun neighbours(vertex: T): Sequence<T> = if (!data.containsKey(vertex)) {
         sequenceOf()
     } else {
-        data[vertex]!!.asSequence()
+        data[vertex]!!.asSequence().map { it.vertex }
     }
+
+    override fun neighboursWithEdgeWeight(vertex: T): Sequence<Graph.VertexWithEdgeWeight<T>> =
+        if (!data.containsKey(vertex)) {
+            sequenceOf()
+        } else {
+            data[vertex]!!.asSequence()
+        }
 
 }
