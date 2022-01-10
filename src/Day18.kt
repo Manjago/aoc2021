64e8609017f2a9d2348857eebfdbf65a502e3d1c
Kirill Temnenkov <kirill@temnenkov.com>
2022-01-10 22:38:57 +0300

    Day18 start parsing, parse 3 simplest cases

--- src/Day18.kt
+++ src/Day18.kt
@@ -10,33 +10,62 @@ fun main() {
         }
     }
 
-    data class SnailfishPair(var left: Snailfish?, var right: Snailfish?) : Snailfish() {
+    data class SnailfishPair(var parent: Snailfish?, var left: Snailfish?, var right: Snailfish?) : Snailfish() {
         override fun toString(): String {
             return "[${left?.toString()},${right?.toString()}]"
         }
     }
 
-    fun parse(strValue: String): Snailfish {
+    fun parse(strValue: String): SnailfishPair {
 
-        val stack = ArrayDeque<SnailfishPair>()
-        val currentNumber = StringBuilder()
+        val head = SnailfishPair(null,null, null)
+        head.parent = head
+        var current: SnailfishPair? = null
+        val sb = StringBuilder()
 
         strValue.toCharArray().asSequence().forEach {
             when {
-                it == '[' -> stack += SnailfishPair(null, null)
-                it.isDigit() -> currentNumber.append(it)
+                it == '[' -> {
+                    when {
+                        current == null -> {
+                            current = head
+                        }
+                        current!!.left == null -> {
+                            val newItem = SnailfishPair(current, null, null)
+                            current!!.left = newItem
+                            current = newItem
+                        }
+                        else -> {
+                            val newItem = SnailfishPair(current, null, null)
+                            current!!.right = newItem
+                            current = newItem
+                        }
+                    }
+                }
+                it.isDigit() -> {
+                    sb.append(it)
+                }
                 it == ',' -> {
-                    stack.last().left = SnailfishNumber(currentNumber.toString().toBigInteger())
-                    currentNumber.clear()
+                    if (sb.isNotEmpty()) {
+                        val justNumber = sb.toString().toBigInteger()
+                        sb.clear()
+                        current!!.left = SnailfishNumber(justNumber)
+                    }
                 }
                 it == ']' -> {
-                    stack.last().right = SnailfishNumber(currentNumber.toString().toBigInteger())
-                    currentNumber.clear()
+                    if (sb.isNotEmpty()) {
+                        val justNumber = sb.toString().toBigInteger()
+                        sb.clear()
+                        current!!.right = SnailfishNumber(justNumber)
+                    }
+                    current = current!!.parent as SnailfishPair
                 }
+
             }
         }
-        return stack.removeFirst()
+        return head
     }
+
 
     fun part1(input: List<String>): Int {
         return input.size
@@ -47,7 +76,6 @@ fun main() {
     }
 
     with(parse("[1,2]")) {
-        check(this is SnailfishPair)
         with(this.left) {
             check(this is SnailfishNumber)
             check(this.value == BigInteger.ONE)
@@ -56,6 +84,51 @@ fun main() {
             check(this is SnailfishNumber)
             check(this.value == BigInteger.TWO)
         }
+        check(this.toString() ==  "[1,2]")
+        println("ok $this")
+    }
+
+    with(parse("[[1,2],3]")) {
+        with(this.left) {
+            check(this is SnailfishPair)
+            with(this) {
+                with(this.left) {
+                    check(this is SnailfishNumber)
+                    check(this.value == BigInteger.ONE)
+                }
+                with(this.right) {
+                    check(this is SnailfishNumber)
+                    check(this.value == BigInteger.TWO)
+                }
+            }
+        }
+        with(this.right) {
+            check(this is SnailfishNumber)
+            check(this.value == BigInteger.valueOf(3L))
+        }
+        check(this.toString() ==  "[[1,2],3]")
+        println("ok $this")
+    }
+
+    with(parse("[9,[8,7]]")) {
+        with(this.left) {
+            check(this is SnailfishNumber)
+            check(this.value == BigInteger.valueOf(9L))
+        }
+        with(this.right) {
+            check(this is SnailfishPair)
+            with(this) {
+                with(this.left) {
+                    check(this is SnailfishNumber)
+                    check(this.value == BigInteger.valueOf(8L))
+                }
+                with(this.right) {
+                    check(this is SnailfishNumber)
+                    check(this.value == BigInteger.valueOf(7L))
+                }
+            }
+        }
+        check(this.toString() ==  "[9,[8,7]]")
         println("ok $this")
     }
 
